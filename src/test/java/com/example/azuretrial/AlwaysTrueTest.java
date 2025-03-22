12133bf515e6d9aac0b26920d0d96c72c3079b58
PrathameshSS <77877305+Prathamesh-007@users.noreply.github.com>
2025-03-23 02:38:45 +0530

    Create AlwaysTrueTest.java

---
+++ src/test/java/com/example/azuretrial/AlwaysTrueTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AlwaysTrueTest {
+
+    @Test
+    void testAlwaysTrue() {
+        assertTrue(true);
+    }
+}
